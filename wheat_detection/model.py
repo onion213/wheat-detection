0b14c16efc57ad7e6ee8610905d782ebe9929972
onion <kosukekito38@gmail.com>
2023-07-17 10:21:35 +0900

    model abstraction

--- wheat_detection/model.py
+++ wheat_detection/model.py
@@ -4,7 +4,25 @@ from torchvision.models.detection import FasterRCNN_ResNet50_FPN_V2_Weights, fas
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 
-class FasterRCNN(pl.LightningModule):
+class Model(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+    def training_step(self, batch):
+        images, targets = batch
+        loss = self.calc_loss(images, targets)
+        return {"loss": loss}
+
+    def calc_loss(self, images, targets):
+        raise NotImplementedError
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(self.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+        return [optimizer], [lr_scheduler]
+
+
+class FasterRCNN(Model):
     def __init__(self, num_classes):
         super().__init__()
         self.num_classes = num_classes
@@ -16,13 +34,7 @@ class FasterRCNN(pl.LightningModule):
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
         self.model = model
 
-    def training_step(self, batch):
-        img, target = batch
-        loss_dict = self.model(img, target)
-        losses = sum(loss for loss in loss_dict.values())
-        return {"loss": losses}
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=0.005, momentum=0.9, weight_decay=0.0005)
-        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-        return [optimizer], [lr_scheduler]
+    def calc_loss(self, images, targets):
+        loss_dict = self.model(images, targets)
+        total_loss = sum(loss for loss in loss_dict.values())
+        return total_loss
